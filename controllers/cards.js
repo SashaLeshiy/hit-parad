2770d014041c1c49b3ece410e1acfd08188a0850
SashaLeshiy <zomlesh@yandex.ru>
2021-10-26 15:58:44 +0300

    фикс регулярки для разбора ссылки

--- controllers/cards.js
+++ controllers/cards.js
@@ -50,7 +50,7 @@ module.exports.createCard = (req, res, next) => {
             image = $('.entity-cover__image').attr('src');
           })
           .then(() => {
-            const re = /\/?/;
+            const re = /[/?]/;
             const songFrame = link.split(re);
             console.log(songFrame);
             const frameSong = `${songFrame[0]}//${songFrame[2]}/iframe/#track/${songFrame[6]}/${songFrame[4]}`;
